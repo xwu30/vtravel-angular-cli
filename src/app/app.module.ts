04b41decee7354becd66a89f3b2920b65ad78f16
Roger Wu <rogerxjwu@outlook.com>
2018-02-24 17:51:33 -0500

    add user form submit function add bootstrap css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {NgMaterialModule} from './ngmaterial.module';
-
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { UserFormComponent } from './components/userform.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,UserFormComponent
   ],
   imports: [
-    BrowserModule,BrowserAnimationsModule,NgMaterialModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent,UserFormComponent],
-  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/userform.component.ts
+++ src/app/components/userform.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'userform',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./userform.component.css']
 })
 export class UserFormComponent {
-  title = 'app';
+  
+
+
+  onSubmit(form:NgForm) {
+    console.log(form);
+  }
 }
